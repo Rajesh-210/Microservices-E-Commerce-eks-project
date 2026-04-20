1022fb8136fad5cd9af1c39b348fb787c63e7d0e
Rajesh-210 <rajeshchilukuri210@gmail.com>
2026-04-20 11:05:16 +0530

    Update backend.tf

--- ecr-terraform/backend.tf
+++ ecr-terraform/backend.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket = "rajesh1"
     key    = "ecr/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-west-1"
   }
 
   required_version = ">= 1.6.3"
