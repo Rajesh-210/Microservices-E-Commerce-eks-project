d592542d98e5f220ed540e9778c5daafdd7cdefb
Rajesh-210 <rajeshchilukuri210@gmail.com>
2026-04-20 11:18:45 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "aluruarumullaa2"
+    bucket = "rajesh1"
     key    = "k8/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-west-1"
   }
 
   required_version = ">= 1.6.3"
